0b0f7b2cd9ef27b056af0d6859cb51902dfc3ca6
Seth <seth.bbrendle@gmail.com>
2022-10-12 10:38:04 -0400

    ruby enumerable debugging

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,51 @@
 # Write your code here
+require "pry"
+
+#-------- using #each --------
+# def reverse_each_word(sentence)
+#     words = sentence.split
+#     reversed_words = []
+
+#     words.each do |word|
+#         reversed_words << word.reverse
+#     end
+
+#     reversed_words.join(" ")
+# end
+
+# binding.pry
+# 0
+
+# -------- Using #map ---------
+# def reverse_each_word(sentence)
+#     words = sentence.split
+
+#     reversed_words = words.map do |word|
+#         word.reverse
+#     end
+
+#     reversed_words.join(" ")
+# end
+
+# binding.pry
+# 0
+
+#-------- Using #map && Proc shorthand --------
+# def reverse_each_word(sentence)
+#     words = sentence.split
+
+#     reversed_words = words.map(&:reverse)
+
+#     reversed_words.join(" ")
+# end
+
+# binding.pry
+# 0
+
+#-------- Method Chaining --------
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+end
+
+binding.pry
+0
